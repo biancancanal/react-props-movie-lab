ee34e3b4483c8906f40160486959104629dced8a
Bianca <biancancanal@gmail.com>
2021-04-18 20:11:49 -0500

    commit tree 9c233679a9526e2138a7107b62a6e4fce01ed6d8

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -5,24 +5,33 @@ import two from '../assets/stars/2-stars.png'
 import three from '../assets/stars/3-stars.png'
 import four from '../assets/stars/4-stars.png'
 import five from '../assets/stars/5-stars.png'
+import MovieShowcase from '../MovieShowcase';
 
 const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 
-export default class CardBack extends Component {
+class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    return (
+      <div>
+          {this.props.IMDBRating === null ? <h4>No Rating Found</h4> : <img src={imgMapper[this.props.IMDBRating]} alt=""/>}
+      </div>
+    )
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title"> {this.props.title} </h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ 
+        this.generateRatingElement()
+        }
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres"> {this.props.genres.join(", ")} </h5>
       </div>
     )
   }
 }
+
+export default CardBack
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -12,6 +12,8 @@ import ttm from '../assets/poster-imgs/the-trash-man.png'
 import React, { Component } from 'react';
 import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
+import MovieShowcase from '../MovieShowcase'
+
 
 
 const posterMap = {
@@ -27,17 +29,33 @@ const posterMap = {
   'default': defaultPoster
 }
 
-export default class MovieCard extends Component {
+class MovieCard extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront 
+          poster = {posterMap[this.props.poster]}
+        />
+        <CardBack 
+          title = {this.props.title}
+          IMDBRating = {this.props.IMDBRating}
+          genres= {this.props.genres} 
+        />
       </div>
     )
   }
 }
 
+export default MovieCard
+
 // Don't forget your default props!
+
+MovieCard.defaultProps = {
+  title: "Unknown",
+  IMDBRating: null,
+  genres: ["No Genre(s) Found"],
+  poster: "default"
+}
